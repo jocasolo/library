604fb066fab78faabd0697ef38c311455a7466b3
jocasolo <jose_csl@hotmail.com>
2016-08-23 17:50:17 +0200

    Creado modelo de la clase User.

---
+++ library/src/main/java/com/at/library/model/User.java
@@ -0,0 +1,57 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 4877910540692673267L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String dni;
+
+	private String name;
+
+	private String surname;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	
+}
