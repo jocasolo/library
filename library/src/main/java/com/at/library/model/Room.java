ff360c87877bc995e7fe8792dc4257074b50fb5f
jocasolo <jose_csl@hotmail.com>
2016-08-24 09:28:58 +0200

    Creado modelo de Room.

---
+++ library/src/main/java/com/at/library/model/Room.java
@@ -0,0 +1,60 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Room implements Serializable {
+
+	private static final long serialVersionUID = -7112342700691944339L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String name;
+
+	private String description;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "room")
+	private List<Shelf> shelves;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Shelf> getShelves() {
+		return shelves;
+	}
+
+	public void setShelves(List<Shelf> shelves) {
+		this.shelves = shelves;
+	}
+
+}
