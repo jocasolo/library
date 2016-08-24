752de26fd665883a1a5b64de9897aed6ce0ae89a
jocasolo <jose_csl@hotmail.com>
2016-08-24 21:11:51 +0200

    Creado UserPutDTO para actualizaciones.

---
+++ library/src/main/java/com/at/library/dto/UserPutDTO.java
@@ -0,0 +1,59 @@
+package com.at.library.dto;
+
+import java.io.Serializable;
+
+public class UserPutDTO implements Serializable {
+
+	private static final long serialVersionUID = 8070095497224083223L;
+
+	private Integer id;
+
+	private String dni;
+
+	private String name;
+
+	private String surname;
+	
+	private AddressDTO address;
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public AddressDTO getAddress() {
+		return address;
+	}
+
+	public void setAddress(AddressDTO address) {
+		this.address = address;
+	}
+
+}
