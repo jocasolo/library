ae99b95e46b91df4245ed4491bdc3f30da85d98c
jocasolo <jose_csl@hotmail.com>
2016-08-24 18:56:41 +0200

    Añadidos a RoomControler métodos para buscar, crear actualizar y borrar.

--- library/src/main/java/com/at/library/controller/RoomController.java
+++ library/src/main/java/com/at/library/controller/RoomController.java
@@ -2,7 +2,11 @@ package com.at.library.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +15,41 @@ import com.at.library.dto.RoomDTO;
 import com.at.library.service.room.RoomService;
 
 @RestController
+@RequestMapping(value = "/room")
 public class RoomController {
 	
 	@Autowired
 	private RoomService roomService;
 	
-	@RequestMapping(value = "/room", method = { RequestMethod.GET })
-	public List<RoomDTO> getAll(){
+	private static final Logger log = LoggerFactory.getLogger(RoomController.class);
+
+	@RequestMapping(method = { RequestMethod.GET })
+	public List<RoomDTO> getAll() {
+		log.debug("Buscando todas las salas en el sistema.");
 		return roomService.findAll();
+	}
+
+	@RequestMapping(method = { RequestMethod.POST })
+	public RoomDTO create(@RequestBody RoomDTO room) {
+		log.debug(String.format("Creando la sala: %s", room));
+		return roomService.create(room);
+	}
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.GET })
+	public RoomDTO findOne(@PathVariable("id") Integer id) {
+		log.debug(String.format("Buscando la sala con id: %s", id));
+		return roomService.findOne(id);
+	}
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.PUT })
+	public void update(@PathVariable("id") Integer id, @RequestBody RoomDTO roomDTO) {
+		log.debug(String.format("Modificando la sala: %s", roomDTO));
+		roomService.update(roomDTO);
+	}
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.DELETE })
+	public void delete(@PathVariable("id") Integer id) {
+		log.debug(String.format("Borrando la sala con el id: %s", id));
+		roomService.delete(id);
 	}
 }
