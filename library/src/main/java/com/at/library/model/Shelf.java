b077b6b1526bbc5be7f3293739e45510e26d7131
jocasolo <jose_csl@hotmail.com>
2016-08-24 09:38:28 +0200

    Añadida referencia de Room a la clase Shelf.

--- library/src/main/java/com/at/library/model/Shelf.java
+++ library/src/main/java/com/at/library/model/Shelf.java
@@ -7,21 +7,27 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Shelf implements Serializable {
 
 	private static final long serialVersionUID = 3010858839926101L;
-	
+
 	@Id
 	@GeneratedValue
 	private Integer id;
-	
+
 	private String description;
-	
+
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "shelf")
 	private List<Book> books;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_id")
+	private Room room;
 
 	public Integer getId() {
 		return id;
@@ -46,5 +52,5 @@ public class Shelf implements Serializable {
 	public void setBooks(List<Book> books) {
 		this.books = books;
 	}
-	
+
 }
