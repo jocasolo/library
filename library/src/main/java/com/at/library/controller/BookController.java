c6618ec5a8fc76577389aa0273baddd13c76641e
jocasolo <jose_csl@hotmail.com>
2016-08-24 18:23:59 +0200

    Añadido en BookController métodos para buscar, crear, actualizar y borrar

--- library/src/main/java/com/at/library/controller/BookController.java
+++ library/src/main/java/com/at/library/controller/BookController.java
@@ -2,7 +2,11 @@ package com.at.library.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +15,42 @@ import com.at.library.dto.BookDTO;
 import com.at.library.service.book.BookService;
 
 @RestController
+@RequestMapping(value = "/book")
 public class BookController {
 
 	@Autowired
 	private BookService bookservice;
 
-	@RequestMapping(value = "/book", method = { RequestMethod.GET })
+	private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
+	@RequestMapping(method = { RequestMethod.GET })
 	public List<BookDTO> getAll() {
+		log.debug("Buscando todos los libros en el sistema.");
 		return bookservice.findAll();
+	}
+
+	@RequestMapping(method = { RequestMethod.POST })
+	public BookDTO create(@RequestBody BookDTO book) {
+		log.debug(String.format("Creando el libro: %s", book));
+		return bookservice.create(book);
+	}
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.GET })
+	public BookDTO findOne(@PathVariable("id") Integer id) {
+		log.debug(String.format("Buscando el libro con id: %s", id));
+		return bookservice.findOne(id);
+	}
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.PUT })
+	public void update(@PathVariable("id") Integer id, @RequestBody BookDTO book) {
+		log.debug(String.format("Modificando el libro: %s", book));
+		bookservice.update(id, book);
+	}
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.DELETE })
+	public void delete(@PathVariable("id") Integer id) {
+		log.debug(String.format("Borrando el libro con el id: %s", id));
+		bookservice.delete(id);
 	}
 
 }
