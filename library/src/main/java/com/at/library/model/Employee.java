0ed968320d14d33174f75a6fec8476163fb39283
jocasolo <jose_csl@hotmail.com>
2016-08-25 19:01:54 +0200

    Creo la clase Employee.

---
+++ library/src/main/java/com/at/library/model/Employee.java
@@ -0,0 +1,71 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = -6958494906030230973L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String dni;
+
+	private String name;
+
+	private String surname;
+
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<User> user = new ArrayList<>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public List<User> getUser() {
+		return user;
+	}
+
+	public void setUser(List<User> user) {
+		this.user = user;
+	}
+
+}
