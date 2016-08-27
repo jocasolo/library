ca971e99e1dc7dbb2ba122ece19618b88243d100
jocasolo <jose_csl@hotmail.com>
2016-08-27 11:00:05 +0200

    Creo findByPkBookAndReturnDateIsNull en RentDAO.

--- library/src/main/java/com/at/library/dao/RentDAO.java
+++ library/src/main/java/com/at/library/dao/RentDAO.java
@@ -1,11 +1,16 @@
 package com.at.library.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.at.library.model.Book;
 import com.at.library.model.Rent;
 
 @Repository
 public interface RentDAO extends CrudRepository<Rent, Integer>{
+	
+	public List<Rent> findByPkBookAndReturnDateIsNull(Book book);
 	
 }
