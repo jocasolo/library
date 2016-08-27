aee3421a46db1394a00acf7f0b5cfbbb595e2417
jocasolo <jose_csl@hotmail.com>
2016-08-27 11:26:09 +0200

    Corregido method de POST a PUT en RentController.

--- library/src/main/java/com/at/library/controller/RentController.java
+++ library/src/main/java/com/at/library/controller/RentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,8 +35,8 @@ public class RentController {
 		return rentService.create(rent);
 	}
 	
-	@RequestMapping(value = "/restore/{idBook}", method = { RequestMethod.POST })
-	public RentDTO restore(Integer idBook){
+	@RequestMapping(value = "/restore/{idBook}", method = { RequestMethod.PUT })
+	public RentDTO restore(@PathVariable("idBook") Integer idBook){
 		log.debug(String.format("Devolviendo alquiler con el libro con id: %s", idBook));
 		return rentService.restore(idBook);
 	}
