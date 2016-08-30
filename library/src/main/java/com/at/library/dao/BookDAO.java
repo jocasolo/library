b4dfadc50367313bc0bc7d4d50ca1f17bf04881c
jocasolo <jose_csl@hotmail.com>
2016-08-30 22:10:35 +0200

    Implementada la búsqueda de libros.

--- library/src/main/java/com/at/library/dao/BookDAO.java
+++ library/src/main/java/com/at/library/dao/BookDAO.java
@@ -12,10 +12,7 @@ import com.at.library.model.Book;
 @Repository
 public interface BookDAO extends CrudRepository<Book, Integer> {
 
-	@Query(value = "SELECT new com.at.library.dto.BookDTO(b.id, b.isbn, b.title, b.author) from Book as b WHERE b.id IN (SELECT r.pk.book.id FROM Rent as r WHERE r.returnDate IS NULL)")
-	public List<BookDTO> findNoAvailable();
-
-	@Query(value = "SELECT new com.at.library.dto.BookDTO(b.id, b.isbn, b.title, b.author) from Book as b WHERE (b.isbn LIKE %?1% OR ?1 IS NULL) AND (b.title LIKE %?2% OR ?2 IS NULL) AND (b.author LIKE %?3% OR ?3 IS NULL) AND status <> 'DELETED'")
+	@Query(value = "SELECT new com.at.library.dto.BookDTO(b.id, b.isbn, b.title, b.author, b.status) from Book as b WHERE (b.isbn LIKE %?1% OR ?1 IS NULL) AND (b.title LIKE %?2% OR ?2 IS NULL) AND (b.author LIKE %?3% OR ?3 IS NULL) AND status <> 'DELETED'")
 	public List<BookDTO> search(String isbn, String title, String author);
 	
 }
