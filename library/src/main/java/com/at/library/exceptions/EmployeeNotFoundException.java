0913bad3a82c4664acd774a7dbfc2c47201dc0d8
jocasolo <jose_csl@hotmail.com>
2016-09-02 15:58:00 +0200

    Creada excepción para empleado no encontrado.

---
+++ library/src/main/java/com/at/library/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,14 @@
+package com.at.library.exceptions;
+
+public class EmployeeNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1969511977784301117L;
+	
+	private static final String MSG = "No se ha encontrado ningún empleado con el Id introducido.";
+
+	@Override
+	public String getMessage() {
+		return MSG;
+	}
+
+}
